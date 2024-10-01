a452c118a4e8dc08bf61c12d54c5fb08c888decf
Vaibhaw Krishna <vaibhaw.krishna.1@gmail.com>
2024-10-01 17:13:06 +0530

    fix

--- app/components/AuthContext.tsx
+++ app/components/AuthContext.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-
+"use client"
 import { createContext, useContext, useState, useEffect, ReactNode } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
